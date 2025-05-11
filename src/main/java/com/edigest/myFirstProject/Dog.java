7c660947922f4eb4775dbd263a37321ea59e1dd5
Vipul Bhatewara <vipul.bhatewara@gmail.com>
2025-05-11 13:35:10 +0530

    First Project

---
+++ src/main/java/com/edigest/myFirstProject/Dog.java
@@ -0,0 +1,12 @@
+package com.edigest.myFirstProject;
+
+import org.springframework.stereotype.Component;
+@Component
+public class Dog {
+
+    public String fun()
+    {
+        return "something";
+    }
+
+}
